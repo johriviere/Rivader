3c4f29ebf85cc605e384ec9c2464b3a98cde168e
jriviere <johriviere@gmail.com>
2020-04-18 19:19:06 +0200

    spaceInvaders coordinates

---
+++ Rivader.Infra/Migrations/20200418171534_SpaceInvaderCoordinates.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rivader.Infra.Migrations
+{
+    public partial class SpaceInvaderCoordinates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Latitude",
+                schema: "dbo",
+                table: "SpaceInvaders",
+                type: "decimal(8,5)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Longitude",
+                schema: "dbo",
+                table: "SpaceInvaders",
+                type: "decimal(8,5)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                schema: "dbo",
+                table: "SpaceInvaders");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                schema: "dbo",
+                table: "SpaceInvaders");
+        }
+    }
+}
