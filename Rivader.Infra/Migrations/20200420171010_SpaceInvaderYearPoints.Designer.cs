0ecfb58f15fcd65f99b5e5b14ebdde71c6137da7
Johann Riviere <50864713+johriviere@users.noreply.github.com>
2020-04-20 19:09:36 +0200

    add year and points to spaceInvader (#8)

--- Rivader.Infra/Migrations/20200420171010_SpaceInvaderYearPoints.Designer.cs
+++ Rivader.Infra/Migrations/20200420171010_SpaceInvaderYearPoints.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rivader.Infra.Storage;
 
 namespace Rivader.Infra.Migrations
 {
     [DbContext(typeof(RivaderDbContext))]
-    partial class RivaderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200420171010_SpaceInvaderYearPoints")]
+    partial class SpaceInvaderYearPoints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
